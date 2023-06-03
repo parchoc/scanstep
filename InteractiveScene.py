054bc7753408997c5d962bc7fcf0973adc6f6484
Konstantin Parhoc <konstantinparhoc@gmail.com>
2023-06-03 14:54:45 +0300

    restructured markupdialog and added lines

--- InteractiveScene.py
+++ InteractiveScene.py
@@ -1,26 +1,32 @@
-from PySide6.QtWidgets import QGraphicsScene, QGraphicsItem, QGraphicsEllipseItem
-from PySide6.QtGui import QPen, QBrush
-from PySide6.QtCore import Qt, Signal
+from PySide6.QtWidgets import QGraphicsScene, QGraphicsItem
+from PySide6.QtCore import Signal, Qt
+from PySide6.QtGui import QBrush, QPen
 
 class InteractiveScene(QGraphicsScene):
     pointAdded = Signal(QGraphicsItem)
 
-    def __init__(self, width=100, height=100, parent=None):
+    def __init__(self, width=100, height=100, parent=None, pen=None, brush=None):
         super().__init__(0, 0, width, height, parent)
-        self.circlePen = QPen()
-        self.circlePen.setWidth(2)
-        self.circleBrush = QBrush(Qt.GlobalColor.red)
-        self.linePen = QPen()
-        self.linePen.setColor(Qt.GlobalColor.red)
-        self.linePen.setWidth(2)
+        if pen:
+            self.circlePen = pen
+        else:
+            self.circlePen = QPen()
+            self.circlePen.setWidth(2)
+        if brush:
+            self.circleBrush = brush
+        else:
+            self.circleBrush = QBrush(Qt.GlobalColor.red)
 
     def mousePressEvent(self, event) -> None:
         self.addPoint(event.scenePos().x(), event.scenePos().y())
         # super().mousePressEvent(event)
         event.accept()
     
-    def addPoint(self, x, y):
-        item = self.addEllipse(0, 0, 6, 6, self.circlePen, self.circleBrush)
+    def addPoint(self, x, y, pen=None, brush=None):
+        if pen and brush:
+            item = self.addEllipse(0, 0, 6, 6, pen, brush)
+        else:
+            item = self.addEllipse(0, 0, 6, 6, self.circlePen, self.circleBrush)
         item.setPos(x - 3, y - 3)
         self.pointAdded.emit(item)
         return item
--- markupdialog.py
+++ markupdialog.py
@@ -1,23 +1,23 @@
-from PySide6.QtCore import Slot, Signal
+from PySide6.QtCore import Slot, Signal, QPointF, Qt
 from ui_markupdialog import Ui_MarkupDialog
 from PySide6.QtWidgets import QDialog, QGraphicsItem, QGraphicsScene
 from InteractiveScene import InteractiveScene
-from PySide6.QtGui import QBrush
+from PySide6.QtGui import QBrush, QPen
 
 CONNECTIONS = {
-    'Y': ('X',),
-    'X': ('Y', 'Z'),
-    'Z': ('X',),
-    'J': [],
-    'F': [],
-    'B': [],
-    'G': [],
-    'A': [],
+    'Y': frozenset(('X', "X'")),
+    'X': frozenset(("Y",)),
+    "X'": frozenset(("Y",)),
+    'Г': frozenset(("З", "L", "B", "N")),
+    'З': frozenset(("Г", "M", "B'")),
+    'B': frozenset(("B'", "Г")),
+    "B'": frozenset(("B", "З")),
+    'A': frozenset(("Y",)),
     'D': [],
     'E': [],
-    'L': [],
-    'M': [],
-    'N': [],
+    'L': frozenset(("Г",)),
+    'M': frozenset(("З",)),
+    'N': frozenset(("Г",)),
 }
 
 class MarkupDialog(QDialog):
@@ -28,6 +28,12 @@ class MarkupDialog(QDialog):
         self.ui = Ui_MarkupDialog()
         self.ui.setupUi(self)
         self.scene = InteractiveScene(backgroundImage.width(), backgroundImage.height())
+        self.circlePen = QPen()
+        self.circlePen.setWidth(2)
+        self.circleBrush = QBrush(Qt.GlobalColor.red)
+        self.linePen = QPen()
+        self.linePen.setColor(Qt.GlobalColor.red)
+        self.linePen.setWidth(1)
         if parameters:
             self.parameters = parameters
         else:
@@ -37,32 +43,67 @@ class MarkupDialog(QDialog):
         if backgroundImage:
             self.scene.addPixmap(backgroundImage)
         # adding point items from initial scene
+        self.points = {}
         if points:
             for key, item in points.items():
-                self.points[key] = self.scene.addPoint(item.x(), item.y())
-                self.points[key].setToolTip(key)
-        else:
-            self.points = {}
+                self.points[key] = self.scene.addPoint(item.x(), item.y(), self.circlePen, self.circleBrush)
+                self.points[key].setToolTip(key)            
+        self.lines = {}
         self.ui.markupView.setScene(self.scene)
         # connections
         self.accepted.connect(self.sendScene)
-        self.scene.pointAdded.connect(self.updatePointsDict)
+        self.scene.pointAdded.connect(self.updateGlobal)
 
     @Slot()
     def sendScene(self):
         self.markupDone.emit(self.scene, self.points, self.parameters)
 
     @Slot(QGraphicsItem)
-    def updatePointsDict(self, item):
-        # remove previous point if it exist
-        if self.ui.pointsBox.currentText() in self.points:
-            self.scene.removeItem(self.points[self.ui.pointsBox.currentText()])
-        # add new point to dict
-        item.setToolTip(self.ui.pointsBox.currentText())
-        self.points[self.ui.pointsBox.currentText()] = item
+    def updateGlobal(self, item):
+        current_point = self.ui.pointsBox.currentText()
+        self.updatePoints(item, current_point)
+        self.updateLines(current_point)
+        # self.updateParameters(current_point)
+        # self.updateParametersDisplay()
         # go to the next point if it not the last one
         if self.ui.pointsBox.currentIndex() < self.ui.pointsBox.count() - 1:
             self.ui.pointsBox.setCurrentIndex(self.ui.pointsBox.currentIndex() + 1)
+
+    @Slot(QGraphicsItem, str)
+    def updatePoints(self, point, name):
+        # remove previous point if it exist
+        if name in self.points:
+            self.scene.removeItem(self.points[name])
+        # add new point to dict
+        point.setToolTip(name)
+        point.setZValue(1)
+        self.points[name] = point
+    
+    @Slot(str)
+    def updateLines(self, updated_point):
+        # go thro all points connected to updated_point
+        for point in CONNECTIONS[updated_point]:
+            # if connected point exists than build line
+            if point in self.points:
+                line = ''.join(sorted(updated_point + point))
+                # remove old line if it exists
+                if line in self.lines:
+                    self.scene.removeItem(self.lines[line])
+                item = self.scene.addLine(
+                    self.points[line[0]].x(),
+                    self.points[line[0]].y(),
+                    self.points[line[1]].x(),
+                    self.points[line[1]].y(),
+                    self.linePen
+                )
+                self.lines[line] = item
+
+    @Slot(str)
+    def updateParameters(self, updated_point):
+        # check if all points for length computetion exist and related point has been modified
+        # length
+        if updated_point in {'Y', "X", "X'"} and len({'Y', "X", "X'"}.intersection(self.points)) == 3:
+            self.lengthFoot()
 
     @Slot()
     def updateParametersDisplay(self):
@@ -79,3 +120,32 @@ class MarkupDialog(QDialog):
             Коэффициент w: {self.parameters['w']}
             '''
         )
+
+    def lengthFoot(self):
+        YX = self.lines['YX'].line()
+        if YX.length() > self.lines["YX'"].line().length():
+            self.parameters['length'] = YX.length()
+        else:
+            # finding perpendicular from X' to YX
+            normal = YX.normalVector()
+            normal.translate(0, YX.length())
+            intersectionPoint = QPointF()
+            YX.intersects(normal, intersectionPoint)
+            # adding intersection point to the scene
+            point = self.scene.addEllipse(0, 0, 6, 6, self.circlePen, self.circleBrush)
+            point.setPos(intersectionPoint.x() - 3, intersectionPoint.y() - 3)
+            if 'Z' in self.points['Z']:
+                self.scene.removeItem(self.points['Z'])
+            self.points['Z'] = point
+            # adding perpendicular line to the scene
+            if 'YZ' in self.lines['YZ']:
+                self.scene.removeItem(self.lines['YZ'])
+                self.lines['YZ'] = self.scene.addLine(
+                    self.points['Y'].x(), 
+                    self.points['Y'].y(), 
+                    point.x(),
+                    point.y(),
+                    self.linePen
+                )
+            self.parameters['length'] = self.lines['YZ'].line().length()
+        return self.parameters['length']
